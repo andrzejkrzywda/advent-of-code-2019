3718a3d50872e0b57fa168084d29eab9fafc026a
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2019-12-17 23:41:10 +0100

    Solution for 2.2

--- restore_the_gravity_assist_program.rb
+++ restore_the_gravity_assist_program.rb
@@ -3,10 +3,14 @@ require_relative 'optcode'
 file = File.open('./input2.txt')
 string_input = file.read
 array_of_numbers = string_input.split(",").map(&:to_i)
-puts array_of_numbers.inspect
 
-array_of_numbers[1] = 12
-array_of_numbers[2] = 2
-
-puts Optcode::Computer.new(array_of_numbers).execute[0]
-# 4714701
+(0..99).each do |i|
+  array_of_numbers[1] = i
+  (0..99).each do |j|
+    array_of_numbers[2] = j
+    result = Optcode::Computer.new(array_of_numbers.clone).execute[0]
+    if result == 19690720
+      puts 100 * i + j
+    end
+  end
+end
