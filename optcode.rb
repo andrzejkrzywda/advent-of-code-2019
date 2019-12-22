a4fe0228628e203d380bc2a93499ddeb883483f1
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2019-12-22 11:29:37 +0100

    5.1 extracted Memory object

--- optcode.rb
+++ optcode.rb
@@ -4,11 +4,11 @@ module Optcode
   class Computer
     def initialize(input, memory)
       @input  = input
-      @memory = memory
+      @memory = Memory.new(memory)
     end
 
     def execute
-      read_slice do |slice|
+      @memory.read_instruction do |slice|
         if slice[0] == 1
           add(slice)
         elsif slice[0] == 2
@@ -16,40 +16,49 @@ module Optcode
         elsif slice[0] == 3
           store_input(slice)
         elsif slice[0] == 99
-          return @memory
+          return @memory.content
         else
           raise UnknownInstruction.new(slice[0])
         end
       end
-      return @memory
+      return @memory.content
     end
 
     private
 
-    def read_slice
-      @pointer = 0
-      while true do
-        if @memory[@pointer] == 3
-          yield(@memory[@pointer..@pointer+1])
-          @pointer += 2
-        else
-          yield(@memory[@pointer..@pointer+3])
-          @pointer += 4
-        end
-      end
-
-    end
-
     def multiply(slice)
-      @memory[slice[3]] = @memory[slice[1]] * @memory[slice[2]]
+      @memory.content[slice[3]] = @memory.content[slice[1]] * @memory.content[slice[2]]
     end
 
     def add(slice)
-      @memory[slice[3]] = @memory[slice[1]] + @memory[slice[2]]
+      @memory.content[slice[3]] = @memory.content[slice[1]] + @memory.content[slice[2]]
     end
 
     def store_input(slice)
-      @memory[slice[1]] = @input.gets.to_i
+      @memory.content[slice[1]] = @input.gets.to_i
     end
   end
+
+  class Memory
+    attr_accessor :content
+
+    def initialize(content)
+      @content = content
+    end
+
+    def read_instruction
+      @pointer = 0
+      while true do
+        if @content[@pointer] == 3
+          yield(@content[@pointer..@pointer+1])
+          @pointer += 2
+        else
+          yield(@content[@pointer..@pointer+3])
+          @pointer += 4
+        end
+      end
+    end
+  end
+
+
 end
